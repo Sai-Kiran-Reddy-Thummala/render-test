b030becae2c699d85bb67dd0a63a2e70fe7a5d36
Sai-Kiran-Reddy-Thummala <saikiranreddythummala@gmail.com>
2024-06-27 15:06:31 +0530

    added gitignore file

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,7 @@ const mongoose = require('mongoose')
 const password = process.argv[2]
 
 // DO NOT SAVE YOUR PASSWORD TO GITHUB!!
-const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
+const url = process.env.MONGODB_URL
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
